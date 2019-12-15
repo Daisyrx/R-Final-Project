d14153aa97d5008ffec5bf890a96f256bc7ef33b
Daisy <daisy@dhcp-wifi-8021x-168-122-217-188.bu.edu>
2019-12-15 16:39:41 -0500

    update 1215

--- Final.R
+++ Final.R
@@ -1,4 +1,31 @@
+# Research questions:
+# The relationship between crime occured and location
+# Does the number of occurence increasing by year
 pacman::p_load(tidyverse,readr,knitr,ggplot2)
 
-# Read the data
+# Read the data 
 crime <- read.csv("Crime Incident.csv")
+# Rename the columns
+colnames(crime) <- c("Incident_Number","Offence_Code","Offence_Code_Group","Description","District","Area",
+                     "Shooting","Occurred_Date","Year","Month","Day_of_Week","Hour","UCR_Part","Street","Latitude",
+                     "Longtitude","Location")
+# Remove the first 7 rows because of the data missing
+crime <- crime[-c(1:7),]
+# have a general idea about the data
+summary(crime)
+
+# Visually see the data
+
+ggplot(crime)+
+  geom_bar(aes(x = Offence_Code_Group,y = ..prop..,group = 1),show.legend = FALSE)
+
+crime$Day_of_Week <- factor(crime$Day_of_Week,
+                            levels = c("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"))
+ggplot(crime)+
+  geom_bar(aes(x=Day_of_Week,y=..prop..,group = 1))
+ggplot(crime)+
+  geom_bar(aes(x=Day_of_Week,fill = District),stat = "count",position = "fill")
+# The plot shows that there are no clear differences in the number of crimes vary from weeks.
+
+
+
