c8aeb7a592438013525c27c92caad71e7a141adf
Daisy <daisy@dhcp-wifi-8021x-168-122-217-188.bu.edu>
2019-12-16 15:45:17 -0500

    1216 update

--- Final.R
+++ Final.R
@@ -1,13 +1,10 @@
 # Research questions:
 # The relationship between crime occured and location
 # Does the number of occurence increasing by year
-pacman::p_load(tidyverse,readr,knitr,ggplot2,magrittr,dplyr)
+pacman::p_load(tidyverse,readr,knitr,ggplot2,magrittr,dplyr,plotly)
 
 # Read the data 
 crime <- read.csv("Crime Incident.csv")
-liquor <- read.csv("liquor-licenses.csv")
-liquor$Address <- tolower(liquor$Address)
-liquor %<>% rename(Street = Address)
 # Rename the columns
 colnames(crime) <- c("Incident_Number","Offence_Code","Offence_Code_Group","Description","District","Area",
                      "Shooting","Occurred_Date","Year","Month","Day_of_Week","Hour","UCR_Part","Street","Latitude",
@@ -16,8 +13,6 @@ colnames(crime) <- c("Incident_Number","Offence_Code","Offence_Code_Group","Desc
 crime <- crime[-c(1:7),]
 # have a general idea about the data
 summary(crime)
-# change street names into lower case
-crime$Street <- tolower(crime$Street)
 # remove missing values
 crime %<>% na.omit()
 # create new column called "City"
@@ -47,19 +42,20 @@ ggplot(motor)+
   geom_bar(aes(x=City,fill = factor(Year)),stat = "count")
 
 # Visually see the data
-ggplot(crime)+
-  geom_bar(aes(x = Offence_Code_Group,y = ..prop..,group = 1),show.legend = FALSE)
+#ggplot(crime)+
+  #geom_bar(aes(x = Offence_Code_Group,y = ..prop..,group = 1),show.legend = FALSE)
 
-ggplot(crime)+
-  geom_bar(aes(x = City),stat = "count",show.legend = FALSE)+
-  coord_flip()
+plotly_build(ggplot(crime)+
+  geom_bar(aes(x = City,fill = factor(Year)),stat = "count",position = "dodge")+
+  coord_flip())
+
+plotly_build(ggplot(crime)+
+               geom_bar(aes(x = Year,fill = factor(City)),stat = "count",position = "dodge")+
+               coord_flip())
 
 crime$Day_of_Week <- factor(crime$Day_of_Week,
                             levels = c("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"))
-ggplot(crime)+
-  geom_bar(aes(x=Day_of_Week,y=..prop..,group = 1))
-ggplot(crime)+
-  geom_bar(aes(x=Day_of_Week,fill = District),stat = "count",position = "fill")
+
 # The plot shows that there are no clear differences in the number of crimes vary from weeks.
 
 # face_wrap by Year to see whether there is a different
@@ -76,11 +72,23 @@ p<-ggplot(crime) +
 p$data <- p$data %>% group_by(City) %>% filter(City == "Brighton")
 p
 
-ggplot(crime)+
-  geom_bar(aes(x=Brighton,y=..prop..,group=1))
 
 ## Prepare groups for mapping
 crime %<>% mutate(group = as.numeric(as.factor(crime$City)))
+cities <- unique(crime$City)
+library(leaflet)
+m <- leaflet(crime) %>% setView(lng = -71.0589, lat = 42.3601, zoom = 12) 
+pal <- colorFactor(c("#999999", "#E69F00", "#56B4E9","#B2182B",
+                     "#D6604D", "#F4A582", "#FDDBC7", "#D1E5F0",
+                     "#92C5DE", "#4393C3", "#2166AC","#FFCC33"), domain = cities)
+map1 <- m %>% addTiles() %>% addCircleMarkers(data = crime,lng = ~Longtitude,lat = ~Latitude,weight = 1,fillOpacity = 0.5,
+                                     radius = 0.5,color = ~pal(City)) %>% 
+  addLegend(position = "bottomright",pal = pal,values = cities,opacity = 1) 
+map1
+
+map2 <- m %>% addTiles() %>%
+  addAwesomeMarkers(data = crime,lng = ~Longtitude,lat = ~Latitude,clusterOptions = T)
+map2
 
 ## Prepare data for Shiny app
 crime_shiny <- crime %>% select(Incident_Number,Offence_Code_Group,City,group,Year,Latitude,Longtitude)
--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 
 library(shiny)
 library(shinydashboard)
-pacman::p_load(tidyverse,readr,knitr,ggplot2,magrittr,dplyr)
+pacman::p_load(tidyverse,readr,knitr,ggplot2,magrittr,dplyr,leaflet,leaflet.extras,maps,plotly)
 crime_shiny <- read.csv("crime_shiny.csv")
 p<-ggplot(crime_shiny) + 
     geom_bar(aes(x = Year,y = ..prop..,fill=Year)) + 
@@ -9,6 +9,14 @@ p<-ggplot(crime_shiny) +
 y <- ggplot(crime_shiny) + 
     geom_bar(aes(x = City, y = ..prop..,group=1)) + 
     facet_wrap(~ Year, nrow = 2)
+pp <- ggplot(crime_shiny)+
+    geom_bar(aes(x = City,fill = factor(Year)),stat = "count",position = "dodge")+
+    coord_flip()
+py <- ggplot(crime_shiny)+
+    geom_bar(aes(x = Year,fill = factor(City)),stat = "count",position = "dodge")+
+    coord_flip()
+    
+cities <- unique(crime_shiny$City)
 
 # Define UI for application that draws a histogram
 
@@ -16,17 +24,29 @@ ui <- dashboardPage(
     dashboardHeader(title = "Crime Incident Reports"),
     dashboardSidebar(
         sidebarMenu(
-            menuItem("Map",tabName = "map",icon = icon("dashboard")),
-            menuItem("City",tabName = "city",icon = icon("th")),
-            menuItem("Year",tabName = "year",icon = icon("th"))
+            menuItem("Map",tabName = "map",icon = icon("map")),
+            menuItem("City",tabName = "city",icon = icon("th"),
+                     menuSubItem("City in General",tabName = "a"),
+                     menuSubItem("City Detail",tabName = "b")),
+            menuItem("Year",tabName = "year",icon = icon("th"),
+                     menuSubItem("Year in General",tabName = "c"),
+                     menuSubItem("Year Detail",tabName = "d"))
         )
     ),
     dashboardBody(
     tabItems(
         tabItem(tabName = "map",
-                h2("Map of Crime Incident")),
-        tabItem(tabName = "city",
+                h2("Map of Crime Incident"),
+                leafletOutput(outputId = "mymap")
+                ),
+        
+        tabItem(tabName = "a",
                 h2("Crime Differ by Cities"),
+                wellPanel(plotlyOutput("CityCount"))
+                ),
+                
+        tabItem(tabName = "b",
+                h2("Cities in Detail"),
                 selectInput("City", "Select a City:", 
                             choices = unique(crime_shiny$City)),
                             
@@ -35,8 +55,14 @@ ui <- dashboardPage(
                             )
         
                  ),
-        tabItem(tabName = "year",
+        
+        tabItem(tabName = "c",
                 h2("Crime Differ by Years"),
+                wellPanel(plotlyOutput("YearCount"))
+        ),
+        
+        tabItem(tabName = "d",
+                h2("Years in Detail"),
                 selectInput("Year", "Select a Year:", 
                             choices = unique(crime_shiny$Year)),
                 
@@ -54,6 +80,20 @@ ui <- dashboardPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
+    pal <- colorFactor(c("#999999", "#E69F00", "#56B4E9","#B2182B",
+                         "#D6604D", "#F4A582", "#FDDBC7", "#D1E5F0",
+                         "#92C5DE", "#4393C3", "#2166AC","#FFCC33"), domain = cities)
+    
+    output$mymap <- renderLeaflet({
+        leaflet(crime_shiny) %>% setView(lng = -71.0589, lat = 42.3601, zoom = 12) %>%
+            addTiles() %>% 
+            addAwesomeMarkers(data = crime_shiny,lng = ~Longtitude,lat = ~Latitude,clusterOptions = T)
+    })
+    
+    output$CityCount <- renderPlotly({
+        plotly_build(pp)
+    })
+    
     CityText <- reactive({
         paste("Selected City:", input$City)
     })
@@ -66,6 +106,10 @@ server <- function(input, output) {
         # plot the bar chart by Cities
         p$data <- p$data %>% group_by(City) %>% filter(City == input$City)
         p
+    })
+    
+    output$YearCount <- renderPlotly({
+        plotly_build(py)
     })
     
     YearText <- reactive({
